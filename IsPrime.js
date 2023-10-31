26f39222586ccf6e87a3c7c90b7aed1dee208659
Mr-React <ambureomkar@gmail.com>
2023-10-31 19:34:20 +0530

    IsPrime program

---
+++ IsPrime.js
@@ -0,0 +1,35 @@
+//Big O - O(n)
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(1));
+console.log(isPrime(5));
+console.log(isPrime(4));
+
+//Big O - O(sqrt(n))
+function isPrimeOptimized(n) {
+  if (n < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrimeOptimized(1));
+console.log(isPrimeOptimized(5));
+console.log(isPrimeOptimized(4));
